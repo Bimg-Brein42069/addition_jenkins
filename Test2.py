17d7e795ac062fb59c20fe7d01bdda7259378fa7
introverted_monke_69 <77499684+Bimg-Brein42069@users.noreply.github.com>
2022-11-20 19:04:26 +0530

    Create Test2.py

---
+++ Test2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import unittest
+from Prog1 import addn
+
+class TestSum(unittest.Testcase):
+    def test_list_int(self):
+        x,y=0,0
+        res = addn(x,y)
+        self.assertEqual(res,0)
+
+if __name__=="__main__":
+    unittest.main()
