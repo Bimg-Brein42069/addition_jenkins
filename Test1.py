0b918fcf67b86ca7bcb481da56e1e9eb5eb397e9
introverted_monke_69 <77499684+Bimg-Brein42069@users.noreply.github.com>
2022-11-25 12:19:23 +0530

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -13,17 +13,17 @@ class TestSum(unittest.TestCase):
         res = addn(x,y)
         self.assertEqual(res,3)
         
-    def test_list_int2(self):
+    def test_list_int3(self):
         x,y=-0.5,1
         res = addn(x,y)
         self.assertEqual(res,0.5)
         
-    def test_list_int2(self):
+    def test_list_int4(self):
         x,y=1,-2
         res = addn(x,y)
         self.assertEqual(res,-1)
         
-    def test_list_int2(self):
+    def test_list_int5(self):
         x,y=-1.5,1
         res = addn(x,y)
         self.assertEqual(res,-0.5)
