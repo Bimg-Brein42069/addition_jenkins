54171fc5b978b87ff0bde4faed7ba726dfee9730
introverted_monke_69 <77499684+Bimg-Brein42069@users.noreply.github.com>
2022-11-25 12:19:06 +0530

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -3,10 +3,30 @@ import unittest
 from Prog1 import addn
 
 class TestSum(unittest.TestCase):
-    def test_list_int(self):
+    def test_list_int1(self):
         x,y=0,0
         res = addn(x,y)
         self.assertEqual(res,0)
+    
+    def test_list_int2(self):
+        x,y=1,2
+        res = addn(x,y)
+        self.assertEqual(res,3)
+        
+    def test_list_int2(self):
+        x,y=-0.5,1
+        res = addn(x,y)
+        self.assertEqual(res,0.5)
+        
+    def test_list_int2(self):
+        x,y=1,-2
+        res = addn(x,y)
+        self.assertEqual(res,-1)
+        
+    def test_list_int2(self):
+        x,y=-1.5,1
+        res = addn(x,y)
+        self.assertEqual(res,-0.5)
 
 if __name__=="__main__":
     unittest.main()
